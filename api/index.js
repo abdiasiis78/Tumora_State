4c231d7c13d7fd08aa5aa85ce2a2e627a5b1e311
abdiasiis78 <gureyxalane78@gmail.com>
2023-11-11 09:57:14 +0300

    databasa-init

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MOONGO).then(() => {
+    console.log("Connected to moongDB")
+}).catch((err) => {
+    console.log(err);
+})
 const app = express()
 
 const port = 3000
