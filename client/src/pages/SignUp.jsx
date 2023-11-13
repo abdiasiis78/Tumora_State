4b2f9ad2cd0be2fb0ce23f2cd2d2ee36cfc70f7a
abdiasiis78 <gureyxalane78@gmail.com>
2023-11-13 11:20:59 +0300

    signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -12,7 +12,7 @@ function SignUp() {
      <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>      
 
     </form>
-    <div className="flex gap-2 mt-5">
+    <div className="flex gap-2 mt-">
       <p>Have an account?</p>
       <Link to='/sign-in'>
         <span  className='text-blue-700'>Sign In</span>
